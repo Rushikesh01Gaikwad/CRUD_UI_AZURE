afe3f393987416367338fff20dd83f2a02aebfd5
Rushikesh01Gaikwad <rushikeshhtc@gmail.com>
2026-04-13 12:33:47 +0530

    hosted on azure

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://yourdomain.com/api/'
+    apiUrl: 'https://crudapi13042026-g0anb6dycudna7ey.centralindia-01.azurewebsites.net/api/'
 };
